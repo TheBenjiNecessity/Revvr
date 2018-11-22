4c85168be760aa99c0b00de4400139542172338b
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-11-21 21:12:00 -0500

    Create extensions for common styles
    as an idea of how to add styles

---
+++ Revvr/Extensions/UIColor+commonStyles.swift
@@ -0,0 +1,21 @@
+//
+//  UIColor+commonStyles.swift
+//  Revvr
+//
+//  Created by Benjamin Wishart on 2018-11-17.
+//  Copyright © 2018 Benjamin Wishart. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+//    static var myAppRed: UIColor {
+//        return UIColor(red: 1, green: 0.1, blue: 0.1, alpha: 1)
+//    }
+//    static var myAppGreen: UIColor {
+//        return UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+//    }
+//    static var myAppBlue: UIColor {
+//        return UIColor(red: 0, green: 0.2, blue: 0.9, alpha: 1)
+//    }
+}
---
+++ Revvr/Extensions/UIFont+commonStyles.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+commonStyles.swift
+//  Revvr
+//
+//  Created by Benjamin Wishart on 2018-11-17.
+//  Copyright © 2018 Benjamin Wishart. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    //    static var myAppTitle: UIFont {
+    //        return UIFont.systemFont(ofSize: 17)
+    //    }
+    //    static var myAppBody: UIFont {
+    //        return UIFont.systemFont(ofSize: 14)
+    //    }
+}
