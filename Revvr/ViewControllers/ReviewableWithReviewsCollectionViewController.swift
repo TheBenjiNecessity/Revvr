cac9c2a0b25a8256b3761d903e447a69fcca0d7d
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-11-23 16:27:14 -0500

    Add button for review creation page

--- Revvr/ViewControllers/ReviewableWithReviewsCollectionViewController.swift
+++ Revvr/ViewControllers/ReviewableWithReviewsCollectionViewController.swift
@@ -22,7 +22,8 @@ class ReviewableWithReviewsCollectionViewController: ReviewsCollectionViewContro
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(addReview))
     }
 
     // MARK: UICollectionViewDataSource
@@ -39,36 +40,18 @@ class ReviewableWithReviewsCollectionViewController: ReviewsCollectionViewContro
         
         return reviewableCollectionReusableView
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    // MARK: Callbacks
+    
+    @objc func addReview() {
+       self.performSegue(withIdentifier: CreateReviewViewController.modalSegueIdentifier, sender: reviewable)
     }
-    */
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let reviewable = sender as? Reviewable {
+            let nav = segue.destination as! UINavigationController
+            let cvc = nav.topViewController as! CreateReviewViewController
+            cvc.setReviewable(reviewable: reviewable)
+        }
+    }
 }
