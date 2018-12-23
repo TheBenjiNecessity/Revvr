ea40607a2c56c4aeb7d159a7a74fa0cb824d2d88
TheBenjiNecessity <benjamin.wishart@gmail.com>
2018-12-22 20:41:14 -0500

    Comment cleanup

--- Revvr/ViewControllers/ReviewTableViewController.swift
+++ Revvr/ViewControllers/ReviewTableViewController.swift
@@ -46,7 +46,7 @@ class ReviewTableViewController: UITableViewController, ReviewActionsDelegate {
         if indexPath.row == 0 {
             let commentHeight = review.comment?.height(withConstrainedWidth: tableView.frame.size.width, font: UIFont.systemFont(ofSize: CGFloat(17.0))) ?? 0
             let rImageHeight = CGFloat(90.0)
-            return margin + ppHeight + margin + rImageHeight + margin + commentHeight + margin //+ height of comment label?
+            return margin + ppHeight + margin + rImageHeight + margin + commentHeight + margin
         } else if indexPath.row > 0 && indexPath.row <= 2 {
             return ppHeight
         } else {
@@ -100,50 +100,4 @@ class ReviewTableViewController: UITableViewController, ReviewActionsDelegate {
         let rrmvc = nav.topViewController as! ReviewReplyModalViewController
         rrmvc.review = review
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
