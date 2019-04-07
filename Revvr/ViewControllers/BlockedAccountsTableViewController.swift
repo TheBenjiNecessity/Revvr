e422344a81f581c8cbd4531e5fafb1252fd78f44
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-04-07 16:11:56 -0400

    Remove comments

--- Revvr/ViewControllers/BlockedAccountsTableViewController.swift
+++ Revvr/ViewControllers/BlockedAccountsTableViewController.swift
@@ -54,10 +54,7 @@ class BlockedAccountsTableViewController: UITableViewController {
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
         let user = sender as! AppUser
         let destination = segue.destination as! UserWithReviewsCollectionViewController
         destination.user = user
