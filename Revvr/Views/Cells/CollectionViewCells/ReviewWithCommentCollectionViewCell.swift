4f5435ded1e46ab1936b56fbe72e1d414e076b2b
TheBenjiNecessity <benjamin.wishart@gmail.com>
2019-05-25 23:08:33 -0400

    Restyle reviews with comments views

--- Revvr/Views/Cells/CollectionViewCells/ReviewWithCommentCollectionViewCell.swift
+++ Revvr/Views/Cells/CollectionViewCells/ReviewWithCommentCollectionViewCell.swift
@@ -17,24 +17,15 @@ fileprivate let masksToBounds = false
 
 fileprivate let paddingConstraint: CGFloat = CGFloat(5.0)
 fileprivate let spacingConstraint: CGFloat = CGFloat(8.0)
+fileprivate let bottomSpacingConstraint: CGFloat = CGFloat(24.0)
 fileprivate let minHeight: CGFloat = CGFloat(120.0)
 
 class ReviewWithCommentCollectionViewCell: ReviewCollectionViewCell {
     @IBOutlet weak var commentLabel: UILabel!
-     @IBOutlet weak var reviewableImageView: UIImageView!
+    @IBOutlet weak var reviewableImageView: UIImageView!
     
     override func setReview(review: Review) {
         super.setReview(review: review)
-        
-        usernameLabel.layer.shadowRadius = shadowRadius
-        usernameLabel.layer.shadowOpacity = shadowOpacity
-        usernameLabel.layer.shadowOffset = shadowOffset
-        usernameLabel.layer.masksToBounds = masksToBounds
-        
-        reviewableLabel.layer.shadowRadius = shadowRadius
-        reviewableLabel.layer.shadowOpacity = shadowOpacity
-        reviewableLabel.layer.shadowOffset = shadowOffset
-        reviewableLabel.layer.masksToBounds = masksToBounds
         
         // It shouldn't be possible for the review comment to be nil so this should be okay
         commentLabel?.text = review.comment!
@@ -43,12 +34,13 @@ class ReviewWithCommentCollectionViewCell: ReviewCollectionViewCell {
     override func getMinHeight(collectionViewWidth: CGFloat) -> CGFloat {
         let ppWidth = profilePictureImageView.frame.size.width
         let lblWidth = collectionViewWidth - (paddingConstraint + ppWidth + paddingConstraint)
+        let reviewableImageHeight = reviewableImageView.frame.size.height
         
         // Compute the height of the username and comment labels based on their widths and text contents
         let profilePictureHeight = profilePictureImageView.frame.size.height
         let commentLblHeight = commentLabel.text?.height(withConstrainedWidth: CGFloat(lblWidth), font: commentLabel.font)
-        let lblHeight = paddingConstraint + profilePictureHeight + commentLblHeight! + paddingConstraint
-        
+        let lblHeight = reviewableImageHeight + paddingConstraint + profilePictureHeight + paddingConstraint + commentLblHeight! + bottomSpacingConstraint
+
         return max(minHeight, lblHeight)
     }
     
